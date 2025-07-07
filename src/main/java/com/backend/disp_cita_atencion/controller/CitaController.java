5e282c16220976f4738a93076bb6f92df7adef02
JpenalozaDuoc <jes.penaloza@duocuc.cl>
2025-07-06 23:09:58 -0400

    Se agregan restricciones por roles de Keycloak

--- src/main/java/com/backend/disp_cita_atencion/controller/CitaController.java
+++ src/main/java/com/backend/disp_cita_atencion/controller/CitaController.java
@@ -5,7 +5,8 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.security.access.prepost.PreAuthorize;
+//import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -23,37 +24,42 @@ import com.backend.disp_cita_atencion.service.CitaService;
 
 @RestController
 @RequestMapping("/api/cita")
-@CrossOrigin(origins = "*")
+//@CrossOrigin(origins = "*")
 public class CitaController {
 
     @Autowired
     private CitaService citaService;
 
     @GetMapping
+    @PreAuthorize("hasAnyRole('ADMIN', 'VETERINARIO', 'ASISTENTE')") // Asistente también necesita ver todos los dueños para buscar
     public ResponseEntity<ApiResponse<List<CitaResponseDTO>>> getAll() {
         List<CitaResponseDTO> citas = citaService.obtenerCitasActivas();
         return ResponseEntity.ok(ApiResponse.exito("Listado exitoso", citas));
     }
 
     @GetMapping("/{id}")
+    @PreAuthorize("hasAnyRole('ADMIN', 'VETERINARIO', 'ASISTENTE')") // Asistente también necesita ver todos los dueños para buscar
     public ResponseEntity<ApiResponse<CitaResponseDTO>> getById(@PathVariable Long id) {
         CitaResponseDTO cita = citaService.buscarCitaPorId(id);
         return ResponseEntity.ok(ApiResponse.exito("Encontrado correctamente", cita));
     }
 
     @PostMapping
+    @PreAuthorize("hasAnyRole('ADMIN', 'VETERINARIO', 'ASISTENTE')") // Asistente también necesita ver todos los dueños para buscar
     public ResponseEntity<ApiResponse<CitaResponseDTO>> create(@RequestBody CitaRequestDTO dto) {
         CitaResponseDTO creado = citaService.crearCita(dto);
         return ResponseEntity.status(HttpStatus.CREATED).body(ApiResponse.exito("Creado correctamente", creado));
     }
 
     @PutMapping("/{id}")
+    @PreAuthorize("hasAnyRole('ADMIN', 'VETERINARIO', 'ASISTENTE')") // Asistente también necesita ver todos los dueños para buscar
     public ResponseEntity<ApiResponse<CitaResponseDTO>> update(@PathVariable Long id, @RequestBody CitaRequestDTO dto) {
         CitaResponseDTO actualizado = citaService.actualizarCita(id, dto);
         return ResponseEntity.ok(ApiResponse.exito("Actualizado correctamente", actualizado));
     }
 
     @DeleteMapping("/{id}")
+    @PreAuthorize("hasAnyRole('ADMIN', 'VETERINARIO', 'ASISTENTE')") // Asistente también necesita ver todos los dueños para buscar
     public ResponseEntity<ApiResponse<Void>> delete(@PathVariable Long id) {
         citaService.eliminarCita(id);
         return ResponseEntity.ok(ApiResponse.exito("Eliminado correctamente", null));
--- src/main/java/com/backend/disp_cita_atencion/controller/DisponibilidadController.java
+++ src/main/java/com/backend/disp_cita_atencion/controller/DisponibilidadController.java
@@ -5,7 +5,8 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.security.access.prepost.PreAuthorize;
+//import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -23,37 +24,42 @@ import com.backend.disp_cita_atencion.service.DisponibilidadService;
 
 @RestController
 @RequestMapping("/api/disponibilidad")
-@CrossOrigin(origins = "*")
+//@CrossOrigin(origins = "*")
 public class DisponibilidadController {
 
     @Autowired
     private DisponibilidadService disponibilidadService;
 
     @GetMapping
+    @PreAuthorize("hasAnyRole('ADMIN', 'VETERINARIO', 'ASISTENTE')") // Asistente también necesita ver todos los dueños para buscar
     public ResponseEntity<ApiResponse<List<DisponibilidadResponseDTO>>> getAll() {
         List<DisponibilidadResponseDTO> lista = disponibilidadService.obtenerDisponibilidadesActivas();
         return ResponseEntity.ok(ApiResponse.exito("Listado exitoso", lista));
     }
 
     @GetMapping("/{id}")
+    @PreAuthorize("hasAnyRole('ADMIN', 'VETERINARIO', 'ASISTENTE')") // Asistente también necesita ver todos los dueños para buscar
     public ResponseEntity<ApiResponse<DisponibilidadResponseDTO>> getById(@PathVariable Long id) {
         DisponibilidadResponseDTO dto = disponibilidadService.buscarPorId(id);
         return ResponseEntity.ok(ApiResponse.exito("Encontrado correctamente", dto));
     }
 
     @PostMapping
+    @PreAuthorize("hasAnyRole('ADMIN', 'VETERINARIO', 'ASISTENTE')") // Asistente también necesita ver todos los dueños para buscar
     public ResponseEntity<ApiResponse<DisponibilidadResponseDTO>> create(@RequestBody DisponibilidadRequestDTO dto) {
         DisponibilidadResponseDTO creado = disponibilidadService.crearDisponibilidad(dto);
         return ResponseEntity.status(HttpStatus.CREATED).body(ApiResponse.exito("Creado correctamente", creado));
     }
 
     @PutMapping("/{id}")
+    @PreAuthorize("hasAnyRole('ADMIN', 'VETERINARIO', 'ASISTENTE')") // Asistente también necesita ver todos los dueños para buscar
     public ResponseEntity<ApiResponse<DisponibilidadResponseDTO>> update(@PathVariable Long id, @RequestBody DisponibilidadRequestDTO dto) {
         DisponibilidadResponseDTO actualizado = disponibilidadService.actualizarDisponibilidad(id, dto);
         return ResponseEntity.ok(ApiResponse.exito("Actualizado correctamente", actualizado));
     }
 
     @DeleteMapping("/{id}")
+    @PreAuthorize("hasAnyRole('ADMIN', 'VETERINARIO', 'ASISTENTE')") // Asistente también necesita ver todos los dueños para buscar
     public ResponseEntity<ApiResponse<Void>> delete(@PathVariable Long id) {
         disponibilidadService.eliminarDisponibilidad(id);
         return ResponseEntity.ok(ApiResponse.exito("Eliminado correctamente", null));
