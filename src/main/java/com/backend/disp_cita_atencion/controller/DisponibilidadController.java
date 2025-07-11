352a9504bb48911591eeb9a957a124ac3c40e2c3
jorge-miranda <jorge.miranda@mysolutions.cl>
2025-07-11 00:39:30 -0400

    Se configure al security config

--- src/main/java/com/backend/disp_cita_atencion/controller/DisponibilidadController.java
+++ src/main/java/com/backend/disp_cita_atencion/controller/DisponibilidadController.java
@@ -8,7 +8,7 @@ import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.CrossOrigin;
+//import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -26,7 +26,7 @@ import com.backend.disp_cita_atencion.service.DisponibilidadService;
 
 @RestController
 @RequestMapping("/api/disponibilidad")
-@CrossOrigin(origins = "*")
+//@CrossOrigin(origins = "*")
 public class DisponibilidadController {
 
     @Autowired
